6ffa1b7bf044e1c36fafa1263862823077374380
Haruka Kotani <harukakotani28@gmail.com>
2021-10-15 18:46:15 -0700

    added delete view

--- BookList/Controllers/BookListController.cs
+++ BookList/Controllers/BookListController.cs
@@ -42,7 +42,7 @@ namespace BookList.Controllers
             return View(obj);
         }
 
-        // GET - Edit
+        // GET - EDIT
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
@@ -56,5 +56,52 @@ namespace BookList.Controllers
             }
             return View(obj);
         }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Book obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Book.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        // GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Book.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Book.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
